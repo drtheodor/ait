62590b49b698cee37b51e4362c0742c71a7bfe37
Loqor <isaiahcrace@gmail.com>
2025-06-17 00:24:28 -0500

    chore: add api notes

--- src/main/java/dev/amble/ait/data/schema/exterior/category/ExclusiveCategory.java
+++ src/main/java/dev/amble/ait/data/schema/exterior/category/ExclusiveCategory.java
@@ -10,6 +10,9 @@ import dev.amble.ait.data.schema.exterior.ExteriorCategorySchema;
 
 /**
  * Used for dev exclusives.
+ * @author Loqor, Theo
+ * @apiNote This category is not meant to be used by players and is reserved for the dev team.
+ * This is NOT meant for multiple variants of the same exterior (cough, Classic). Every dev team member gets one exterior.
  */
 public class ExclusiveCategory extends ExteriorCategorySchema {
     public static final Identifier REFERENCE = AITMod.id("exterior/exclusive");
