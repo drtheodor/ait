2a60a5a5cdeddcfa684fb621b8b3f78828cd1208
alex <70163067+rapbattlegod32@users.noreply.github.com>
2025-07-30 21:33:35 +0100

    feat: an option in the ait-config client that will disable or enable the darkness effect that you get in the tardis if the power is turned off

--- src/main/java/dev/amble/ait/client/util/FoggyUtils.java
+++ src/main/java/dev/amble/ait/client/util/FoggyUtils.java
@@ -2,6 +2,7 @@ package dev.amble.ait.client.util;
 
 import com.mojang.blaze3d.systems.RenderSystem;
 
+import dev.amble.ait.client.AITModClient;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.render.FogShape;
 import net.minecraft.client.render.OverlayTexture;
@@ -62,12 +63,14 @@ public class FoggyUtils {
 
         if (tardis.isGrowth()
                 || ClientTardisUtil.getPowerDelta() != ClientTardisUtil.MAX_POWER_DELTA_TICKS) {
+            if (!AITModClient.CONFIG.powerOffDarkness) return;
             RenderSystem.setShaderFogStart(MathHelper.lerp(ClientTardisUtil.getPowerDeltaForLerp(), -8, 24));
             RenderSystem.setShaderFogEnd(MathHelper.lerp(ClientTardisUtil.getPowerDeltaForLerp(), 11, 32));
             RenderSystem.setShaderFogShape(FogShape.SPHERE);
             RenderSystem.setShaderFogColor(0, 0, 0, tardis.siege().isActive() ? 0.85f : 1f);
         }
         if (tardis.crash().isToxic() && tardis.fuel().hasPower()) {
+
             RenderSystem
                     .setShaderFogStart(MathHelper.lerp(MinecraftClient.getInstance().getTickDelta() / 100f, -8, 24));
             RenderSystem.setShaderFogEnd(MathHelper.lerp(MinecraftClient.getInstance().getTickDelta() / 100f, 11, 32));
