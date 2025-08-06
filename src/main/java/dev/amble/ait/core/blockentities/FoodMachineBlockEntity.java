2c3600cabd2914515f0988d875550365db0c15b7
alex <70163067+rapbattlegod32@users.noreply.github.com>
2025-08-06 03:55:44 +0100

    mnoved var to top and made methods for fuel/power

--- src/main/java/dev/amble/ait/core/blockentities/FoodMachineBlockEntity.java
+++ src/main/java/dev/amble/ait/core/blockentities/FoodMachineBlockEntity.java
@@ -1,12 +1,13 @@
 package dev.amble.ait.core.blockentities;
 
+import dev.amble.ait.api.tardis.link.v2.block.InteriorLinkableBlockEntity;
 import net.minecraft.block.BlockState;
-import net.minecraft.block.entity.BlockEntity;
 import net.minecraft.util.math.BlockPos;
 
 import dev.amble.ait.core.AITBlockEntityTypes;
 
-public class FoodMachineBlockEntity extends BlockEntity {
+public class FoodMachineBlockEntity extends InteriorLinkableBlockEntity {
+    private FoodMachineMode currentMode = FoodMachineMode.FOOD_CUBES;
 
     public FoodMachineBlockEntity(BlockPos pos, BlockState state) {
         super(AITBlockEntityTypes.FOOD_MACHINE_BLOCK_ENTITY_TYPE, pos, state);
@@ -18,14 +19,20 @@ public class FoodMachineBlockEntity extends BlockEntity {
         OVERCHARGED_FOOD_CUBES
     }
 
-    private FoodMachineMode currentMode = FoodMachineMode.FOOD_CUBES;
-
     public void setMode(FoodMachineMode mode) {
         this.currentMode = mode;
     }
 
     public FoodMachineMode getMode() {
         return currentMode;
+    }
+
+    public void eatFuel(double value){
+        this.tardis().get().fuel().removeFuel(value);
+    }
+
+    public boolean isPoweredOn(){
+        return this.tardis().get().fuel().hasPower();
     }
 
 }
